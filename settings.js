4570bf3c30fac986d20601e88dceff2960577dff
XxX-Nike-XxX <94451510+XxX-Nike-XxX@users.noreply.github.com>
2023-06-03 21:09:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'Kuhaku'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "antumulum@gmail.com" //email
+global.email_password = "iqbalmudin27fc" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
